984bf4f61234802983a6562b434212cf58a03134
thomas-luizon <thomaslrgregorio@gmail.com>
2022-12-24 13:14:38 -0300

    feat: added bufferline

--- lua/thomas/plugins-setup.lua
+++ lua/thomas/plugins-setup.lua
@@ -99,6 +99,9 @@ return packer.startup(function(use)
 	-- git integration
 	use("lewis6991/gitsigns.nvim") -- show line modifications on left hand side
 
+	-- bufferline
+	use("akinsho/bufferline.nvim")
+
 	if packer_bootstrap then
 		require("packer").sync()
 	end
